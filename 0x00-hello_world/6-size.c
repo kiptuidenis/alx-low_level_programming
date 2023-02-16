e7f9a58745c6c30473476539c15ff412c5982457
kiptuidenis <kiptuidenis@gmail.com>
2023-02-16 05:29:57 -0800

    Modified file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ long int long_int;
 long long int long2_int;
 float float_size;
 
-printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(charsize));
+printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(char_size));
 printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(int_size));
 printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(long_int));
 printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(long2_int));
