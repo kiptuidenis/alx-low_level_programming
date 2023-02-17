45be32a88ca32779339d7b0e2d0a3cea722bfa7f
kiptuidenis <kiptuidenis@gmail.com>
2023-02-17 22:45:13 +0300

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,9 @@
 int main(void)
 {
 	int i = 0;
-	char alphabet[26] = {'0', '1', '2', '3', '4', '5', '6', '7', '8',
+	char alphabet[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8',
 			       '9', 'a', 'b', 'c', 'd', 'e', 'f',};
-	for (i = 0; i <= 25; i++)
+	for (i = 0; i <= 15; i++)
 	{
 		putchar(alphabet[i]);
 	}
