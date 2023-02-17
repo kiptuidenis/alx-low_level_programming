2a48925e064306a390cdaa3fa0858368b8851d4d
kiptuidenis <kiptuidenis@gmail.com>
2023-02-17 21:16:00 +0300

    Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+	char alphabet[26] = {'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'q', 'r', 's', 't'
+			     , 'u', 'v', 'w', 'x', 'y', 'z'};
+	for (i = 0; i <= 25; i++)
+	{
+		putchar(alphabet[i]);
+	}
+
+	putchar('\n');
+	return (0);
+}
