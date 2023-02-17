2a334a0eccf5820c2db2c7bcc511d622800e73b1
kiptuidenis <kiptuidenis@gmail.com>
2023-02-17 20:48:10 +0300

    small alphabet

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/** Prints the last digit of number n */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	
+}
