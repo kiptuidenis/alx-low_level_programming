310f6da47d55498dfc533a9d3d422c6ae32c8ab1
kiptuidenis <kiptuidenis@gmail.com>
2023-02-18 09:34:05 +0300

    File modified

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = -1;
+
+	while (++i < 10)
+	{
+		putchar(i + '0');
+
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
