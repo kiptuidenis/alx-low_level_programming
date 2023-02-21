1b9da303d0c049fce5c5b04260a5ce1755e8de7c
kiptuidenis <kiptuidenis@gmail.com>
2023-02-21 22:33:23 +0300

    Task 10.a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *add - adds two integers
+ *@a: first argument
+ *@b: second argument
+ *
+ *Return: result of addition
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,4 +5,5 @@ int _putchar(char c);
 int _isalpha(int c);
 int print_sign(int n);
 void print_to_98(int n);
+int add(int a, int b);
 void print_alphabet_x10(void);
