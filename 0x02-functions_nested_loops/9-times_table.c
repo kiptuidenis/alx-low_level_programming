f6614a2c5bb20798666125022266eb53ae9d9bb6
kiptuidenis <kiptuidenis@gmail.com>
2023-02-21 23:19:51 +0300

    Task 9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *times_table - prints timetable
+ */
+
+void times_table(void);
+{
+	int tmp = 0;
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		int num = 0;
+		int num2;
+
+		printf("%d", num);
+		putchar(',');
+		putchar(' ');
+
+		for (j = 0; j <= 8; j++)
+		{
+			num2 = num + tmp;
+
+			printf("%d", num2);
+
+			if (j < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		tmp = tmp + 1;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,4 +6,5 @@ int _isalpha(int c);
 int print_sign(int n);
 void print_to_98(int n);
 int add(int a, int b);
+void times_table(void);
 void print_alphabet_x10(void);
