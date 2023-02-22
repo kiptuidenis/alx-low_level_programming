309dfad71a4f86b2224553c05659da5c5de1b3ff
kiptuidenis <kiptuidenis@gmail.com>
2023-02-22 16:08:55 +0300

    Task 9 correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include "main.h"
 
 /**
  *times_table - prints timetable
@@ -22,7 +23,7 @@ void times_table(void)
 			{
 				_putchar('0');
 			}
-			if (product < 10)
+			else if (product < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
