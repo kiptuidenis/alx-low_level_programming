04eb731a54a24f6c433aa388583d7d927e1d9769
kiptuidenis <kiptuidenis@gmail.com>
2023-02-22 16:05:44 +0300

    Task 9 correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,29 +8,35 @@
 
 void times_table(void)
 {
-	int i;
-	int j;
+	int row, column, product, tens, ones;
 
-	for (i = 0; i <= 9; i++)
+	for (row = 0; row <= 9; row++)
 	{
-		int num;
-
-		for (j = 0; j <= 9; j++)
+		for (column = 0; column <= 9; column++)
 		{
-			num = i * j;
-			printf("%d", num);
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
 
-			if (j < 9)
+			if (column == 0)
 			{
-				putchar(',');
-				if (num < 10)
-				{
-					putchar(' ');
-				}
-				putchar(' ');
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
 			}
 		}
-
-		putchar('\n');
+		_putchar('\n');
 	}
 }
