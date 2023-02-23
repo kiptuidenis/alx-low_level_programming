d439e89b10617fcf717d4062bc33d91c734f99f5
kiptuidenis <kiptuidenis@gmail.com>
2023-02-23 16:27:44 +0300

    0.isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,20 +3,22 @@
 #include <time.h>
 
 /**
- *_isalpha - checks if argument passed is alphabet
+ *_isupper - checks if argument passed is alphabet
  *@c: The character to be checked
  *
  *Return: 1 if the letter is lowercase or uppercase. 0 otherwise
  */
 
-int _isalpha(int c)
+int _isupper(int c)
 {
 	int tmp = c;
 
-	if ((tmp >= 97 && tmp <= 122) || (tmp >= 65 && tmp <= 90))
+	if (tmp >= 97 && tmp <= 122)
+	{
+		return (0);
+	}
+	else if (tmp >= 65 && tmp <= 90)
 	{
 		return (1);
 	}
-	else
-		return (0);
 }
