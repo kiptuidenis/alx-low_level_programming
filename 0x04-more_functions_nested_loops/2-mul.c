88d7aca574e3d1f36c1914e8d56dd11b2c308b8b
kiptuidenis <kiptuidenis@gmail.com>
2023-02-23 16:39:05 +0300

    Task 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *mul - multiplies 2 integers
+ *@a: 1st integer
+ *@b: 2nd integer
+ *Return: 1 if the letter is lowercase or uppercase. 0 otherwise
+ */
+
+int mul(int a, int b)
+{
+	int tmp;
+
+	tmp = a * b;
+
+	return (tmp);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _putchar(char c);
 int _isdigit(int c);
+int mul(int a, int b);
 int _isupper(int c);
