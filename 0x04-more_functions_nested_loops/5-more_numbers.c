5fa1ee265ccc748fd977f33aecd784f304a8fd3d
kiptuidenis <kiptuidenis@gmail.com>
2023-02-23 17:19:01 +0300

    Task 5. Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *more_numbers - prints 0-14 10x
+ */
+
+void more_numbers(void)
+{
+	int numb[20] = {0,1,2,3,4,5,6,7,8,9,1,0,1,1,1,2,1,3,1,4};
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 19)
+		{
+			_putchar(numb[j] + '0');
+		}
+
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@ int _putchar(char c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void more_numbers(void);
 int _isupper(int c);
