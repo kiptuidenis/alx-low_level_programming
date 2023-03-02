1ecf9efbea3b4052946aa3fb1c5f4d92e24a1bef
kiptuidenis <kiptuidenis@gmail.com>
2023-03-02 10:19:57 +0300

    Task 7.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ *leet - encodes letters to numbers
+ *@str: The string to be encoded
+ *
+ *Return: encoded string
+ */
+
+char *leet(char *str)
+{
+	int str_len = 0;
+
+	while (str[str_len] != '\0')
+	{
+		str_len++;
+	}
+
+	char *encoded_str = (char *)malloc(sizeof(char) * (str_len + 1));
+
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+			encoded_str[i] = '4';
+		else if (str[i] == 'e' || str[i] == 'E')
+			encoded_str[i] = '3';
+		else if (str[i] == 'o' || str[i] == 'O')
+			encoded_str[i] = '0';
+		else if (str[i] == 't' || str[i] == 'T')
+			encoded_str[i] = '7';
+		else if (str[i] == 'l' || str[i] == 'L')
+			encoded_str[i] = '1';
+		else
+			encoded_str[i] = str[i];
+		i++;
+
+	}
+
+	encoded_str[i] = '\0';
+
+	return (encoded_str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 char *_strcat(char *dest, char *src);
+char *leet(char *str);
 int _putchar(char c);
