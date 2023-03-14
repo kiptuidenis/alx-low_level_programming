caded3aa514835ea736d4e3c2b779eb8faedc100
kiptuidenis <kiptuidenis@gmail.com>
2023-03-14 09:31:56 +0300

    Modified main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,1 +1,16 @@
 int main(int argc, char *argv[]);
+int _putchar(char c);
+
+#include <unistd.h>
+
+/**
+ *_putchar - writes character c to stdout
+ *@c: the character to print
+ *
+ *Return: On success 1, on error -1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
