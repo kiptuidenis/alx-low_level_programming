43616be8f90364bf91d686348827251e29a2ae47
kiptuidenis <kiptuidenis@gmail.com>
2023-03-14 12:46:24 +0300

    Advanced Task. Modified file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,13 +16,13 @@ int main(int argc, char *argv[])
 	int coins[5] = {25, 10, 5, 2, 1};
 	int num_to_change;
 
-	num_to_change = atoi(argv[1]);
-
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num_to_change = atoi(argv[1]);
 
 	if (num_to_change <= 0)
 	{
@@ -30,9 +30,9 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	for (i = 0; i < 5; i++)
+	for (i = 0; i <= 4; i++)
 	{
-		while (num_to_change > coins[i])
+		while (num_to_change >= coins[i])
 		{
 			num_to_change = num_to_change - coins[i];
 			change++;
