cb5f509a6dc1f1ba5d12e4586e4292a3f21e0c44
kiptuidenis <kiptuidenis@gmail.com>
2023-03-14 10:14:22 +0300

    Task 2- Print all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - main function
+ *@argc: argument count
+ *@argv: array of arguments
+ *Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
