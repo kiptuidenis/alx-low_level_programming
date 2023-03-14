f2a6e578ecd01e36e3fb6b1c3ef62e803c69c515
kiptuidenis <kiptuidenis@gmail.com>
2023-03-14 10:07:14 +0300

    Modified 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,11 @@
  *Return: 0 if success
  */
 
-int main(int argc, char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
+
+	(void) argv;
 
 	return (0);
 }
