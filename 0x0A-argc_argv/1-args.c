d804e0b9b539056635354630c4e75e8c0c88f03d
kiptuidenis <kiptuidenis@gmail.com>
2023-03-14 09:54:42 +0300

    Task 1. No of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - main function
+ *@argc: argument count
+ *@argv: array of arguments
+ *Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
