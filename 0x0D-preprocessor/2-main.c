30a7bd875e8b7c3d8091a0554738d09b53b660ee
kiptuidenis <kiptuidenis@gmail.com>
2023-03-29 19:20:27 +0300

    Task 2.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+
+/**
+ *main - Prints name of this file
+ *Return: Success
+ */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+
+	return (0);
+}
