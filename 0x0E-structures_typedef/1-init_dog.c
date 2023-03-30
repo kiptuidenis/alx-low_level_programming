cb15664bb3722e7f14da02ff55ac3529b86150fe
kiptuidenis <kiptuidenis@gmail.com>
2023-03-30 21:23:58 +0300

    Task 1.

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include "dog.h"
-
-
 /**
  * init_dog - Initializes structure
  * @d: struct
@@ -16,18 +12,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    struct dog my_dog;
-
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
 }
