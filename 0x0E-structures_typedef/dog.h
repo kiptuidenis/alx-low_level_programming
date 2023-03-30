c030ada258060af905ca30e4b79df07f8f2fe639
kiptuidenis <kiptuidenis@gmail.com>
2023-03-30 20:59:17 +0300

    Task 0.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - has attributes of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner:Owner of the dog
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
