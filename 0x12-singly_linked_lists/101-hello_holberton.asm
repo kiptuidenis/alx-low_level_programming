e9fc6aff8c39df7649120662c2b6ce1e720a18e5
kiptuidenis <kiptuidenis@gmail.com>
2023-03-31 10:08:20 +0300

    Task 6.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,13 @@
- global    main
-          extern    printf
+section .data
+    msg db 'Hello, Holberton', 0Ah, 0
+
+section .text
+    global main
+    extern printf
+
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+    mov rdi, msg
+    xor rax, rax
+    call printf
+    xor rax, rax
+    ret
