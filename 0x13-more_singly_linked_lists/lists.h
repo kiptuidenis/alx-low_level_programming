c9b8585528872d57de2bc7d813509dc5ec0cde99
kiptuidenis <kiptuidenis@gmail.com>
2023-04-03 17:16:25 +0300

    Task 11.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -28,6 +28,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
 
 #include "0-print_listint.c"
 #include "1-listint_len.c"
@@ -40,5 +41,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 #include "8-sum_listint.c"
 #include "9-insert_nodeint.c"
 #include "10-delete_nodeint.c"
+#include "100-reverse_listint.c"
+
 
 #endif
