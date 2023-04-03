43f2dd8048d9df0e2e94c528f13d87af5a46af6e
kiptuidenis <kiptuidenis@gmail.com>
2023-04-03 15:56:45 +0300

    Task 4.

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees a listint_t list
+ * @head: Pointer to the head of the list to be freed
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current_node;
+
+	while (head)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node);
+	}
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,10 +20,12 @@ typedef struct listint_s
 #include "1-listint_len.c"
 #include "2-add_nodeint.c"
 #include "3-add_nodeint_end.c"
+#include "4-free_listint.c"
 size_t print_listint(const listint_t *h);
 int _putchar(char c);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint_t(listint_t *head);
 
 #endif
