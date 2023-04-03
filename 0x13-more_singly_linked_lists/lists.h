64f99083f6ebf7996a833cb575a26be2453a7069
kiptuidenis <kiptuidenis@gmail.com>
2023-04-03 15:27:58 +0300

    Task 2.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 
 #define LISTS_H
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  *@n: integer
@@ -17,8 +18,10 @@ typedef struct listint_s
 
 #include "0-print_listint.c"
 #include "1-listint_len.c"
+#include "2-add_nodeint.c"
 size_t print_listint(const listint_t *h);
 int _putchar(char c);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
