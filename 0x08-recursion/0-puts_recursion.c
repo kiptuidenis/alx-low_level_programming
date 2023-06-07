d367929d83f91506e1cafe4c132b31d7e2c38e99
kiptuidenis <kiptuidenis@gmail.com>
2023-06-08 01:49:57 +0300

    Task 0-attempt 3

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,9 +11,11 @@ void _puts_recursion(char *s)
 {
 	_putchar(*s);
 
-	if (*s != '\0')
-		_puts_recursion(s + 1);
-
 	if (*s == '\0')
 		_putchar('\n');
+
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+	}
 }
