f281e36ded6bc45b7c513f3b6937925c0984ba0a
kiptuidenis <kiptuidenis@gmail.com>
2023-06-08 10:46:05 +0300

    Task 4 - powers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *_pow_recursion - finds the power of a value
+ *@x: number whose power is to be calculated
+ *@y: power to be raised to.
+ *
+ *Return: power of x
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
