4ecb9731de5a7a8b4509c1299b0bc7fed2e94aea
kiptuidenis <kiptuidenis@gmail.com>
2023-06-19 12:40:44 +0300

    Tas 0- dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - dog structure
+ *@name:  name of dog
+ *@age: age of dog
+ *@owner: Owner of dog
+ *
+ *Description:Structure of type dog that contains the name,
+ *age and owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
