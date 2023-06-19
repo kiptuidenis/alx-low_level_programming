0b9c32b34c08729c56b4a8b6b77b2d6d5badb926
kiptuidenis <kiptuidenis@gmail.com>
2023-06-19 15:38:33 +0300

    Task 2- print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,43 +11,21 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		printf("%s", "");
-
-	if (d->name == NULL && &(d->age) != NULL && d-> owner != NULL)
-	{
+	if (d->name == NULL && &(d->age) != NULL && d->owner != NULL)
 		printf("Name: (nil)\nAge: %.6f\nOwner: %s\n", d->age, d->owner);
-	}
-
-	if (&(d->age) == NULL && d-> owner != NULL && d-> name != NULL)
-	{
+	if (&(d->age) == NULL && d->owner != NULL && d->name != NULL)
 		printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
-	}
-
 	if (d->owner == NULL && &(d->age) != NULL && d->name != NULL)
-	{
 		printf("Name: %s\nAge: %.6f\nOwner: (nil)\n", d->name, d->age);
-	}
-
 	if (d->name == NULL && d->owner == NULL && &(d->age) == NULL)
-	{
 		printf("Name: (nil)\nAge: (nil)\nOwner: (nil)\n");
-	}
-
 	if (d->name == NULL && d->owner == NULL && &(d->age) != NULL)
-	{
 		printf("Name: (nil)\nAge: %.6f\nOwner: (nil)\n", d->age);
-	}
-
 	if (d->name == NULL && d->owner != NULL && &(d->age) == NULL)
-	{
 		printf("Name: (nil)\nAge: (nil)\nOwner: %s\n", d->owner);
-	}
-
 	if (d->name != NULL && d->owner == NULL && &(d->age) == NULL)
-	{
 		printf("Name: %s\nAge: (nil)\nOwner: (nil)\n", d->name);
-	}
-
-	if (d->name != NULL && d->owner != NULL && &(d->age)!= NULL)
+	if (d->name != NULL && d->owner != NULL && &(d->age) != NULL)
 	{
 		printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age,
 		       d->owner);
