b72e1e4956eb39b0111a889e6316a9848baf339b
kiptuidenis <kiptuidenis@gmail.com>
2023-06-20 14:05:32 +0300

    Task 0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - prints the name of file
+ *
+ *Return: 0 on Success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This repo contains tasks on arguments to main
