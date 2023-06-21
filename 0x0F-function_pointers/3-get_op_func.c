6a6583fe87089f863262a0c73b5fb5647db99bdb
kiptuidenis <kiptuidenis@gmail.com>
2023-06-21 17:05:16 +0300

    Calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,12 +23,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
-	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
+        while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
 
-	return (NULL);
+
+	return (ops[i].f);
 }
