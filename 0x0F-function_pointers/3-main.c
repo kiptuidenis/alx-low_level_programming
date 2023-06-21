624cf4aa2588101313d2d8ae83f1071ff7ce43f9
kiptuidenis <kiptuidenis@gmail.com>
2023-06-21 19:11:27 +0300

    status

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -31,8 +31,8 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0)
-	    || (*op == '%' && num2 == 0))
+	if ((*op == '/' && b == 0)
+	    || (*op == '%' && b == 0))
 	{
 		printf("Error\n");
 		exit(100);
