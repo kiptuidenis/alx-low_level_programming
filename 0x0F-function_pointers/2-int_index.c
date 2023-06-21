8f4876bc038b388da4b3ee0b78bfedbf62f72b2e
kiptuidenis <kiptuidenis@gmail.com>
2023-06-21 13:01:10 +0300

    all at once

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,7 +7,8 @@
  *@size: number of elements in array
  *@cmp: function to compare
  *
- *Return: Index of first element,-1 if no element matches or if size less than 0
+ *Return: Index of first element,-1 if no element matches
+ *or if size less than 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
