5b91fed186bae494feba898ac38bbcad96c14c41
kiptuidenis <kiptuidenis@gmail.com>
2023-06-30 22:04:37 +0300

    Prints message before main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *print_message - Prints message before main function
+ *
+ */
+
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
