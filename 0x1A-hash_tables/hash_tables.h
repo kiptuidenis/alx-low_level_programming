e3254e24d05e0862e6c5b1670df4dbe17dee1291
kiptuidenis <kiptuidenis@gmail.com>
2023-11-03 17:16:53 +0300

    Updates

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -16,7 +16,6 @@ typedef struct hash_table_s {
 
 /* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
-
-/* Add more function prototypes as needed */
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /* HASH_TABLES_H */
