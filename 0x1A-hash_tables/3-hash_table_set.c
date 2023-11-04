f2b7751958c11c8b452956a6e34a572f457d2710
kiptuidenis <kiptuidenis@gmail.com>
2023-11-04 12:17:39 +0300

    Updates

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     {
         return (0);
     }
-    index = key_index(key, ht->size);
+    index = key_index((const unsigned char *)key, ht->size);
 
     new_elem = malloc(sizeof(hash_node_t));
     if (new_elem == NULL)
@@ -28,9 +28,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         return (0);
     }
 
-    new_elem->key = key;
+    new_elem->key = (char *)key;
     new_elem->next = NULL;
-    new_elem->value = value;
+    new_elem->value = (char *)value;
 
     if (ht->array[index] == NULL)
     {
