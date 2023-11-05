a23843c177a9778ede5eab9aadc94fc5cb28ee84
kiptuidenis <kiptuidenis@gmail.com>
2023-11-05 17:30:35 +0300

    Updates

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -6,32 +6,30 @@
  * hash_table_get - Retrieves a value given a Key
  * @ht: hash table to be searched
  * @key: Key
- * 
+ *
  * Return: Value if present else NULL
 */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *temp;
+	unsigned long int index;
+	hash_node_t *temp;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-    {
-        return (NULL);
-    }
+	if (ht == NULL || key == NULL || *key == '\0')
+	{
+		return (NULL);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
 
-    index = key_index((const unsigned char *)key, ht->size);
-    temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			return (temp->value);
+		}
+		temp = temp->next;
+	}
 
-    while (temp)
-    {
-        if (strcmp(temp->key, key) == 0)
-        {
-            return (temp->value);
-        }
-        temp = temp->next;
-    }
-
-    return (NULL);
-    
+	return (NULL);
 }
