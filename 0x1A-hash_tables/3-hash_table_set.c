4e39bdae32ae4385eeb0828421a38610a97b3cd0
kiptuidenis <kiptuidenis@gmail.com>
2023-11-05 17:23:54 +0300

    Updates

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,25 +13,26 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
+	char *new_value;
 	hash_node_t *new_node, *current;
-    int check;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
-
-    /*1. Checks if an index is occupied
-     *2. If it is occupied, it traverses the list cheking if key already exists
-     *3. If key exists, it updates the value corresponding to key and returns 1
-     *4. Breaks out of loop if key does not exist
-    */
+	/**
+	 *1. Checks if an index is occupied
+	 *2. If it is occupied, it traverses the list cheking if key already
+	 *exists
+	 *3. If key exists, it updates the value corresponding
+	 *to key and returns 1
+	 *4. Breaks out of loop if key does not exist
+	 */
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
-			char *new_value = strdup(value);
+			new_value = strdup(value);
 			if (new_value == NULL)
 				return (0);
 			free(current->value);
@@ -45,10 +46,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-    check = set_value(new_node, key, value);
-    if (check == 0)
-        return (0);
-
+	if (set_value(new_node, key, value) == 0)
+		return (0);
     /*Insert new node at the beginning of the list*/
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
@@ -59,8 +58,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  * set_value - copies values to the table
  * @key: Key to be copied
  * @value: Value to be copied
- * @ht: Hash table
- * 
+ * @new_node: new node created. Represents the new element to be inserted.
+ *
  * Return: 1 on success, 0 otherwise
 */
 
@@ -82,5 +81,5 @@ int set_value(hash_node_t *new_node, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-    return (1);
+	return (1);
 }
