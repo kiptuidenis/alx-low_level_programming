be2455369585d7b6ea9d1e629388a2913c8f36cb
kiptuidenis <kiptuidenis@gmail.com>
2023-12-23 12:58:28 +0300

    Updates

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""This is a technical interview preparation
+"""
+
+def island_perimeter(grid):
+    """That returns the perimeter of the island described in grid
+
+    Args:
+        grid (list): List of integers
+    """
+    perim = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                x = i - 1
+                y = j
+                if x < 0:
+                    perim += 1
+                else:
+                    if grid[x][y] == 0:
+                        perim += 1
+                x = i
+                y = j - 1
+                if y < 0:
+                    perim += 1
+                else:
+                    if grid[x][y] == 0:
+                        perim += 1
+                x = i
+                y = j + 1
+                if y >= len(grid[i]):
+                    perim += 1
+                else:
+                    if grid[x][y] == 0:
+                        perim += 1
+                x = i + 1
+                y = j
+                if y >= len(grid[i]):
+                    perim += 1
+                else:
+                    if grid[x][y] == 0:
+                        perim += 1
+    return perim
