85c9b29904e80a0ce6dae87fc72fe8bbbe7a1ae8
kiptuidenis <kiptuidenis@gmail.com>
2023-12-31 12:56:32 +0300

    Updates

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,38 +8,43 @@ def island_perimeter(grid):
     Args:
         grid (list): List of integers
     """
-    if len(grid) > 100:
-        return
-    perim = 0
+    total_perim = 0
+    square_perim = 0
+    check_lake = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 x = i - 1
                 y = j
                 if x < 0:
-                    perim += 1
+                    square_perim += 1
                 else:
                     if grid[x][y] == 0:
-                        perim += 1
+                        square_perim += 1
                 x = i
                 y = j - 1
                 if y < 0:
-                    perim += 1
+                    square_perim += 1
                 else:
                     if grid[x][y] == 0:
-                        perim += 1
+                        square_perim += 1
                 x = i
                 y = j + 1
                 if y >= len(grid[i]):
-                    perim += 1
+                    square_perim += 1
                 else:
                     if grid[x][y] == 0:
-                        perim += 1
+                        square_perim += 1
                 x = i + 1
                 y = j
                 if y >= len(grid[i]):
-                    perim += 1
+                    square_perim += 1
                 else:
                     if grid[x][y] == 0:
-                        perim += 1
-    return perim
+                        square_perim += 1
+            total_perim += square_perim
+            if square_perim == 3:
+                check_lake += 1
+    if check_lake < 2:
+        return "Check Lake"
+    return total_perim
